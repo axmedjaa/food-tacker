3c5a2625ee426ec832242e1b78a4a2a8aa1a706e
axmedjaa <axmedja44@gmail.com>
2026-03-14 14:13:21 +0300

    updated server file and and readmi file

--- server.ts
+++ server.ts
@@ -6,6 +6,7 @@ import cors from "cors"
 import authrouter from "./router/auth.router.js"
 import footRouter from "./router/foot.js"
 import reportsRouter from "./router/reports.js"
+import { Request, Response, NextFunction } from "express";
 const app=express()
 app.use(cors())
 app.use(express.json())
@@ -13,6 +14,15 @@ app.use(express.urlencoded({extended:true}))
 app.use("/api/auth",authrouter)
 app.use("/api/food",footRouter)
 app.use("/api/reports",reportsRouter)
-app.get("/health",(req,res)=>res.send("ok"))
+// error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal server error" });
+});
+
+// 404 middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: "Route not found" });
+});
 connectDb()
 app.listen(process.env.PORT||3000,()=>console.log("server is running on "+process.env.PORT))
